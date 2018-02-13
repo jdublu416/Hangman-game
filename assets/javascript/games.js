f2f652e0b9542fbe652f1d2a7caf402740e20930
Janet Warren <33846232+jdublu416@users.noreply.github.com>
2018-02-13 17:20:02 -0500

    ongoing code writing of hangman game

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -3,21 +3,56 @@
 console.log("clicked1");
 var words = ['dog', 'tree', 'swing', 'doctor', 'around', 'scientist', 'jazz', 'hover', 'flower', 'emergency'];
 
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var hiddenWord = [];
+
+
 // press any key to get started with the game
 
 document.onkeyup = function (event) {
-         
+
     console.log("clicked2");
 
     // This function will pick our word
     function pickedWord(words) {
-        // Write code here
+
         return words[Math.floor(Math.random() * words.length)];
 
-        // return to the html with format for game  "_ _ _" (corresponding to word length)  
+        // return to the hidden variable with format for game  "_ _ _" (corresponding to word length)  
 
     }
-    var html = pickedWord(words);
+
+    // assign the word from pickedWord function to a variable
+
+    var hidden = pickedWord(words);//need to take this variable value and feed it into the hiddenWord array
+
+
+    // document.getElementById("demo").innerHTML = text;
+
+    //make an array for each letter in the picked word value
+    var activeWord = hidden.split("");
+    var text = "";
+    for (var i = 0; i < activeWord.length; i++) {
+
+        text += activeWord[i]
+    }
+    alert("I'm split");
+    console.log(activeWord);
+
+    //make a function to take the hidden word array and make not visible
+    function hideWord(activeWord) {
+
+        for (var i in activeWord) {
+
+            hiddenWord[i] = hideWord
+}
+
+    }
+
+
 
     // Set the inner HTML contents of the #game div to our html string
     document.querySelector("#marquis").innerHTML = html;

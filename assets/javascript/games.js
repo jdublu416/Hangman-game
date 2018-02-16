bf0756ff0060c1721833a1dddb32000a10e49709
Janet Warren <33846232+jdublu416@users.noreply.github.com>
2018-02-16 10:47:16 -0500

    changes made to hw, update the repo

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,60 +1,58 @@
 
 // This array holds the words we are going to choose from.
 console.log("clicked1");
-var words = ['dog', 'tree', 'swing', 'doctor', 'around', 'scientist', 'jazz', 'hover', 'flower', 'emergency'];
+var words = ['dog', 'sand', 'ocean', 'castle', 'volleyball', 'wave', 'jazz', 'cruise', 'yacht', 'chaise','tides',
+    'sunburn', 'sunscreen', 'tropical', 'marguerita', 'dacquiri', 'mermaid', 'carribean', 'frisbee'];
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var hiddenWord = [];
-
-
 // press any key to get started with the game
 
 document.onkeyup = function (event) {
 
-    console.log("clicked2");
+    console.log("button heard");
 
     // This function will pick our word
     function pickedWord(words) {
 
         return words[Math.floor(Math.random() * words.length)];
 
-        // return to the hidden variable with format for game  "_ _ _" (corresponding to word length)  
-
+       
     }
 
     // assign the word from pickedWord function to a variable
 
     var hidden = pickedWord(words);//need to take this variable value and feed it into the hiddenWord array
-
-
-    // document.getElementById("demo").innerHTML = text;
+    var html = hidden;
+   
 
     //make an array for each letter in the picked word value
     var activeWord = hidden.split("");
     var text = "";
     for (var i = 0; i < activeWord.length; i++) {
 
-        text += activeWord[i]
-    }
-    alert("I'm split");
-    console.log(activeWord);
-
-    //make a function to take the hidden word array and make not visible
-    function hideWord(activeWord) {
-
-        for (var i in activeWord) {
-
-            hiddenWord[i] = hideWord
-}
-
+         text += activeWord[i]
     }
 
+    // loop to use array from activeWord and put in html to display div, create new span with css class dressing, which will make each letter hidden
+    for (var a in activeWord) {
+        
+        var targetDiv = document.getElementById("display");;
+        var newSpan = document.createElement("span");
+        newSpan.innerHTML = activeWord[a];
+        targetDiv.appendChild(newSpan);
+        newSpan.setAttribute("class", "dressing");
+
+    }
+
+    //create function for button click
+
+    
+
+   
+    }
 
 
-    // Set the inner HTML contents of the #game div to our html string
-    document.querySelector("#marquis").innerHTML = html;
 
-}

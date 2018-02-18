56f0a1d85ac26298e195bbf3c348b1e164543cd4
Janet Warren <33846232+jdublu416@users.noreply.github.com>
2018-02-18 18:27:07 -0500

    final homework submission--Thank You for the extra time!

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,87 +1,131 @@
-
 // This array holds the words we are going to choose from.
 // console.log("clicked1");
-var words = ['dog', 'sand', 'ocean', 'castle', 'volleyball', 'wave', 'jazz', 'cruise', 'yacht', 'chaise', 'tides',
-    'sunburn', 'sunscreen', 'tropical', 'marguerita', 'dacquiri', 'mermaid', 'carribean', 'frisbee'];
+//declare all global variables
+var words = [
+  "dog",
+  "sand",
+  "ocean",
+  "castle",
+  "volleyball",
+  "wave",
+  "jazz",
+  "cruise",
+  "yacht",
+  "chaise",
+  "tides",
+  "sunburn",
+  "sunscreen",
+  "tropical",
+  "marguerita",
+  "dacquiri",
+  "mermaid",
+  "carribean",
+  "frisbee",
+  "jetski",
+  "boat",
+  "surf",
+  "currents",
+  "dolphin",
+  "snorkel"
+];
 
 //var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-   // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    //'t', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+// 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+//'t', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var activeWord = "";
-
-
-var guessCount= 9;// set this to 9 (instead of 8 to offset the initial click event;
+var button = "";
+var hidden = "";
+var guessedLetter = "";
+var letter = "";
+var lossCount = 0;
+var guessCount = 8; // set this to 9 (instead of 8 to offset the initial click event;
+var letterCount;
+var winCount = 0;
 
 // press any key to get started with the game
+document.onkeyup = function(event) {
+  console.log("button heard"); //alert to console that the button was heard
+  hidden = pickedWord(words); //need to take this variable value and feed it into the hiddenWord array
 
-document.onkeyup = function (event) {
+  console.log(hidden); //tells me what the "hidden word" is
 
-    console.log("button heard");
+  //make an array for each letter in the picked word value
+  activeWord = hidden.split("");
+  var text = "";
+  for (var i = 0; i < activeWord.length; i++) {
+    text += activeWord[i];
+    console.log(activeWord[i]);
+  }
 
-    // This function will pick our word
-    function pickedWord(words) {
+  // loop to use array from activeWord and put in html to display div, create new span with css class dressing, which will make each letter hidden
 
-        return words[Math.floor(Math.random() * words.length)];
-
-
-    }
-
-    // assign the word from pickedWord function to a variable
-
-    var hidden = pickedWord(words);//need to take this variable value and feed it into the hiddenWord array
-    // var html = hidden;
-
-    console.log(hidden);
-
-
-    //make an array for each letter in the picked word value
-    activeWord = hidden.split("");
-    var text = "";
-    for (var i = 0; i < activeWord.length; i++) {
-
-        text += activeWord[i];
-        console.log(activeWord[i]);
-        return activeWord;
-    }
-
-    // loop to use array from activeWord and put in html to display div, create new span with css class dressing, which will make each letter hidden
-    for (var a =0; a < activeWord.length; a++) {
-
-        var targetDiv = document.getElementById("display");
-        var newSpan = document.createElement("span");
-        newSpan.innerHTML = activeWord[a];
-        targetDiv.appendChild(newSpan);
-        newSpan.setAttribute("class", "dressing");
-        newSpan.setAttribute("value", activeWord[a]);    // <--------------------add this to each span
-    
-    }
-
-    userGuess(event);
-
+  for (var a = 0; a < activeWord.length; a++) {
+    var targetDiv = document.getElementById("display");
+    var newSpan = document.createElement("span");
+    letterCount = activeWord.length;
+    newSpan.innerHTML = activeWord[a];
+    targetDiv.appendChild(newSpan);
+    newSpan.setAttribute("class", "dressing");
+    newSpan.setAttribute("name", activeWord[a]); // <--------------------add this to each span
+  }
 };
 
 function userGuess(event) {
-    //need to compare the button value to the value of the the span in the html
-    //also need the button to add a class that will change bgcolor and display the letter in the span
-    var button = event.target;
-    var letter = button.innerHTML;
-    
-    if (activeWord [a] === letter) {
+  //need to compare the button value to the value of the the span in the html
+  //also need the button to add a class that will change bgcolor and display the letter in the span
 
-        button.setAttribute("class", "clicked");
-        document.getElementsByValue(letter).classList.remove("dressing");
-        document.getElementsByValue(letter).classList.add("reveal");
-          //second console output
-        console.log(letter);
-            
-    }
-     else {
+  button = event.target;
+  var letter = button.innerHTML;
+  var listGuess = document.getElementsByName(letter);
+  button.setAttribute("class", "clicked");
 
-        button.setAttribute("class", "clicked");
-        guessCount--;
-        console.log(guessCount);
-    }
- }
+  for (var i = 0; i < listGuess.length; i++) {
+    var span = listGuess[i];
+    span.setAttribute("class", "reveal");
+  }
 
+  if (listGuess.length === 0) {
+    guessCount--;
+    Check(guessCount);
+    displayGuessCount(guessCount);
+    changeImage(guessCount);
+  } else if (listGuess.length > 0) {
+    letterCount = letterCount - listGuess.length;
+    console.log(letterCount);
+    letterCountCheck(letterCount);
+  }
+
+  console.log(guessCount);
+}
+
+function Check(guessCount) {
+  if (guessCount === 0) {
+    document.getElementById("text1").innerHTML = "YOU LOST! GAME OVER...";
+  }
+}
+
+function letterCountCheck(letterCount) {
+  if (letterCount === 0) {
+    document.getElementById("text1").innerHTML = "YOU WIN!";
+    winCount++;
+    displayWinCount(winCount);
+  }
+}
+
+function pickedWord(words) {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function displayGuessCount(guessCount) {
+  document.getElementById("guessesleft").innerHTML =
+    "guesses remaining:" + guessCount;
+}
+
+function changeImage(guessCount) {
+  var newSrc = "assets/images/hangmanhung" + guessCount + ".jpg";
+  document.getElementById("sequence").src = newSrc;
+}
+function displayWinCount(winCount) {
+  document.getElementById("wins").innerHTML = "Win Record:" + winCount;
+}
